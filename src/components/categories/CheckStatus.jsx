d6bb72a96fe0ed993e009a8fb7688f76f3347b33
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-03-31 18:08:19 +0100

    Add check status button to categories page

---
+++ src/components/categories/CheckStatus.jsx
@@ -0,0 +1,9 @@
+const CheckStatus = () => (
+  <div>
+    <button type="button" className="check-status">
+      Check status
+    </button>
+  </div>
+);
+
+export default CheckStatus;
--- src/routes/Categories.jsx
+++ src/routes/Categories.jsx
@@ -1,6 +1,9 @@
+import CheckStatus from '../components/categories/CheckStatus';
+
 const Categories = () => (
   <>
     <h1>Under construction</h1>
+    <CheckStatus />
   </>
 );
 export default Categories;
