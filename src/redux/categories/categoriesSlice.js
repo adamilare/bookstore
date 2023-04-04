e664829c9060a43d32d8a5b06632225df7b45b85
Damilare Adepoju <108327427+adamilare@users.noreply.github.com>
2023-04-04 11:37:57 +0100

    Add Redux slices

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: 'Under construction',
+};
+
+const checkStatusReducer = (state) => {
+  if (state.categories && state.categories.lenght < 1) {
+    state.categories.status = 'Under construction';
+  }
+};
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: [],
+  reducers: {
+    checkStatus: checkStatusReducer,
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
